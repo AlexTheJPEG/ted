a88c00a0ab72de8ba7ef0f222efa52bbd0960c0b
AlexTheJPEG <alexphan85120@gmail.com>
2019-12-22 15:34:53 -0500

    Add langcodes command

--- cogs/translate.py
+++ cogs/translate.py
@@ -6,7 +6,6 @@ from googletrans.constants import LANGUAGES
 class Translate(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.langcodes = list(LANGUAGES.keys())
 
     @commands.command()
     async def translate(self, ctx, src, des, phrase):
@@ -19,9 +18,16 @@ class Translate(commands.Cog):
         pass
 
     @commands.command()
-    async def langcodes(self, ctx, src, des, phrase):
-        mention = ctx.message.author.mention
-        pass
+    async def langcodes(self, ctx):
+        author = ctx.message.author
+        codes = [f"{key.rjust(5)} - {value.title()}" for (key, value) in LANGUAGES.items()]
+        formatted_codes = '\n'.join(codes)
+
+        # Send message
+        await author.send(f"""Here are a list of codes you can use for `{ctx.prefix}translate` and `{ctx.prefix}badtranslate`:
+```
+{formatted_codes}
+```""")
 
 
 def setup(client):
