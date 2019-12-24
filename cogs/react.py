be7c2e9b6035e9345f159efa7b2e9a451bc8057d
AlexTheJPEG <alexphan85120@gmail.com>
2019-12-24 11:08:18 -0500

    Put yesno emotes into list

--- cogs/react.py
+++ cogs/react.py
@@ -4,14 +4,15 @@ from discord.ext import commands
 class React(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.yesno_emotes = ["👍", "👎"]
         self.number_emotes = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
 
     @commands.command()
     async def yesno(self, ctx, question=""):
         if question != "":
             poll = await ctx.send(question)
-            await poll.add_reaction("👍")
-            await poll.add_reaction("👎")
+            for emote in self.yesno_emotes:
+                await poll.add_reaction(emote)
         else:
             await ctx.send("What is your question?")
 
