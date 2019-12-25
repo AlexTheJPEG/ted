dfefd3cd727933224bb04aa36bbc1b51d01ae793
AlexTheJPEG <alexphan85120@gmail.com>
2019-12-25 09:37:26 -0500

    Add first server join command

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,6 +9,14 @@ class Fun(commands.Cog):
     async def thegame(self, ctx):
         await ctx.send("I lost The Game.", tts=True)
 
+    @commands.command()
+    async def whendidijoin(self, ctx):
+        mention = ctx.message.author.mention
+        if ctx.guild is None:
+            await ctx.send("It seems you are not using this command on a server. Go to a server and use this command again!")
+        else:
+            await ctx.send(f"{mention} Your very first time joining **{ctx.guild.name}** was on {ctx.author.joined_at}!")
+
 
 def setup(client):
     client.add_cog(Fun(client))
