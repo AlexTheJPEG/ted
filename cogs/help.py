72c5d9a6a940c3309ecbab70c0812a9e1ad416ba
AlexTheJPEG <alexphan85120@gmail.com>
2019-12-25 08:58:58 -0500

    Setup help command for use with JSON

--- cogs/help.py
+++ cogs/help.py
@@ -1,22 +1,26 @@
 import discord
+import json
+import os
 from discord.ext import commands
 
 
 class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.help_texts_path = os.getcwd() + "/cogs/"
+        self.help_texts = json.loads(open(self.help_texts_path + 'help.json', 'r').read())
 
     @commands.command()
     async def help(self, ctx, command=""):
         command_names = [command.name for command in self.client.commands]
+        author = ctx.message.author
+        embed = discord.Embed(colour=13399553)
+        embed.set_image(url="https://cdn.discordapp.com/avatars/457280200078524421/a66cc42e800589c1296fcc2665270e62.png")
 
         # TODO: Make individual help texts for commands
         if command != "" and command not in command_names:
             await ctx.send(f"That's not a valid command! Type `{ctx.prefix}help` for a list of commands to get help on.")
         elif command == "" or command in "help":
-            author = ctx.message.author
-            embed = discord.Embed(colour=13399553)
-            embed.set_image(url="https://cdn.discordapp.com/avatars/457280200078524421/a66cc42e800589c1296fcc2665270e62.png")
             embed.set_author(name="Ted's List of Commands", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
             embed.add_field(name="PING", value="`ping`")
@@ -28,7 +32,13 @@ class Help(commands.Cog):
             await author.send(content=f"""To get help on a specific command, type `{ctx.prefix}help (command)`
 To bring up this message again, type `{ctx.prefix}help`!""", embed=embed)
         else:
-            print(command)
+            embed.set_author(name=f"Ted's List of Commands >>> {command}", url="https://www.youtube.com/watch?v=sMKoNBRZM1M")
+
+            embed.add_field(name="USAGE", value=self.help_texts[command]["usage"])
+            embed.add_field(name="ARGUMENTS", value=self.help_texts[command]["arguments"])
+            embed.add_field(name="WHAT IT DOES", value=self.help_texts[command]["what-it-does"])
+
+            await author.send(content=f"""To bring up this message again, type `{ctx.prefix}help {command}`!""", embed=embed)
 
 
 def setup(client):
