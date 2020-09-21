ffda15973e7e88f3b10d897349933e386dafeace
alexthejpeg <alexphan85120@gmail.com>
2020-09-21 13:08:20 -0400

    Get rid of unnecessary lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,8 @@ import pytest
 
 @pytest.fixture()
 async def bot():
-    bot = commands.Bot(command_prefix="$", help_command=None)
+    bot = commands.Bot(command_prefix="$")
 
     dpytest.configure(bot)
-
-    test_guild = dpytest.backend.make_guild("Test Guild")
-    dpytest.backend.make_text_channel("general", test_guild)
-    test_user = dpytest.backend.make_user("TestUser", "8008")
-    dpytest.backend.make_member(test_user, test_guild, "TestUser")
 
     yield bot
