708d29d3a82da2d9a9b69a28b45503b45588d8ae
alexthejpeg <alexphan85120@gmail.com>
2020-12-24 11:59:10 -0500

    Remove tests (for now)

--- noxfile.py
+++
@@ -1,7 +0,0 @@
-import nox
-
-
-@nox.session(python=["3.8", "3.7", "3.6"])
-def tests(session):
-    session.run("pipenv", "install", external=True)
-    session.run("pipenv", "run", "test", external=True)
